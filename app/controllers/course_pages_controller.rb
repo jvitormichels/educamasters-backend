bd2555b8647bd6e3c9dedda6ec582d279d8d623c
jvitormichels <jvitormichels@gmail.com>
2024-12-03 10:18:48 -0300

    feat: ajuste do backend para suportar crud de páginas de cursos

---
+++ app/controllers/course_pages_controller.rb
@@ -0,0 +1,52 @@
+class CoursePagesController < ApplicationController
+  before_action :set_page, only: [:show, :update, :destroy]
+
+  def index
+    pages = CoursePage.where(course_id: params[:course_id]).order(position: :asc)
+
+    render json: {
+      pages: pages.map(&:format_api)
+    }
+  end
+
+  def show
+    render json: @page.format_api
+  end
+
+  def create
+    byebug
+    page = CoursePage.create(page_params)
+
+    render json: page.format_api, status: :created
+  end
+
+  def update
+    @page.update(update_page_params)
+
+    render json: @page.format_api
+  end
+
+  def destroy
+    byebug
+    @page.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_page
+    byebug
+    id = params[:id].to_i
+    @page = CoursePage.find_by(id: id)
+
+    raise CoursePageException::NotFoundForIdError.new(id) unless @page.present?
+  end
+
+  def page_params
+    params.require(:page).permit(:title, :video, :position, :course_id)
+  end
+
+  def update_page_params
+    params.require(:page).permit(:title, :position, :course_id)
+  end
+end
--- app/models/course.rb
+++ app/models/course.rb
@@ -4,14 +4,22 @@ class Course < ApplicationRecord
 
   mount_uploader :thumbnail, CourseThumbnailUploader
 
+  has_many :course_pages, dependent: :destroy
+
   def format_api
     {
       id: id,
       name: name,
       description: description,
       end_date: end_date.to_date,
-      thumbnail: thumbnail_url
+      thumbnail: thumbnail_url,
+      pages: get_course_pages
     }
+  end
+
+  def get_course_pages
+    # será alterado para ordenar de acordo com o parâmetro position, mas por hora sem tempo irmão
+    self.course_pages.order(created_at: :asc).map(&:format_api)
   end
 
   def thumbnail_url
