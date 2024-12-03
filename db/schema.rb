0b8dacfab72b5996299c03e2e614f9026ba7597d
jvitormichels <jvitormichels@gmail.com>
2024-12-03 16:05:02 -0300

    feat: API de estatísticas para dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_03_053904) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_03_134334) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_03_053904) do
     t.string "video"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "file_size"
   end
 
   create_table "courses", force: :cascade do |t|
